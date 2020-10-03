792f22212924cd6b9057d4b34358cd9a557b6939
dchristofolli <danielchristofolli@gmail.com>
2020-10-03 20:27:22 -0300

    incluindo JSON property

--- src/main/java/com/dchristofolli/finalgrades/v1/dto/Nota.java
+++ src/main/java/com/dchristofolli/finalgrades/v1/dto/Nota.java
@@ -1,37 +1,34 @@
 package com.dchristofolli.finalgrades.v1.dto;
 
+import com.fasterxml.jackson.annotation.JsonGetter;
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonSetter;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class Nota {
     private Long id;
-    private Double Prova1;
-    private Double Prova2;
-    private Double Prova3;
-    private Double peso1;
-    private Double peso2;
-    private Double peso3;
+    @JsonProperty("Prova1")
+    private Double test1;
+    @JsonProperty("Prova2")
+    private Double test2;
+    @JsonProperty("Prova3")
+    private Double test3;
 
-    public Nota(Long id, Double prova1, Double prova2, Double prova3, Double peso1, Double peso2, Double peso3) {
+    public Nota(Long id, Double test1, Double test2, Double test3) {
         this.id = id;
-        Prova1 = prova1;
-        Prova2 = prova2;
-        Prova3 = prova3;
-        this.peso1 = peso1;
-        this.peso2 = peso2;
-        this.peso3 = peso3;
+        this.test1 = test1;
+        this.test2 = test2;
+        this.test3 = test3;
     }
 
     @Override
     public String toString() {
         return "Nota{" +
             "id=" + id +
-            ", Prova1=" + Prova1 +
-            ", Prova2=" + Prova2 +
-            ", Prova3=" + Prova3 +
-            ", peso1=" + peso1 +
-            ", peso2=" + peso2 +
-            ", peso3=" + peso3 +
+            ", Prova1=" + test1 +
+            ", Prova2=" + test2 +
+            ", Prova3=" + test3 +
             '}';
     }
 
@@ -43,51 +40,33 @@ public class Nota {
         this.id = id;
     }
 
-    public Double getProva1() {
-        return Prova1;
+    @JsonGetter("Prova1")
+    public Double getTest1() {
+        return test1;
     }
 
-    public void setProva1(Double prova1) {
-        Prova1 = prova1;
+    @JsonSetter("Prova1")
+    public void setTest1(Double test1) {
+        this.test1 = test1;
     }
 
-    public Double getProva2() {
-        return Prova2;
+    @JsonGetter("Prova2")
+    public Double getTest2() {
+        return test2;
     }
 
-    public void setProva2(Double prova2) {
-        Prova2 = prova2;
+    @JsonSetter("Prova2")
+    public void setTest2(Double test2) {
+        this.test2 = test2;
     }
 
-    public Double getProva3() {
-        return Prova3;
+    @JsonGetter("Prova3")
+    public Double getTest3() {
+        return test3;
     }
 
-    public void setProva3(Double prova3) {
-        Prova3 = prova3;
-    }
-
-    public Double getPeso1() {
-        return peso1;
-    }
-
-    public void setPeso1(Double peso1) {
-        this.peso1 = peso1;
-    }
-
-    public Double getPeso2() {
-        return peso2;
-    }
-
-    public void setPeso2(Double peso2) {
-        this.peso2 = peso2;
-    }
-
-    public Double getPeso3() {
-        return peso3;
-    }
-
-    public void setPeso3(Double peso3) {
-        this.peso3 = peso3;
+    @JsonSetter("Prova3")
+    public void setTest3(Double test3) {
+        this.test3 = test3;
     }
 }
